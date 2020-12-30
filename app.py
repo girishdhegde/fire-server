33d25f2b3a51ea1d38d79990741bae8eee56e192
Tushar pharale <42889275+tusharip@users.noreply.github.com>
2020-12-31 00:23:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,18 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 UPLOAD_FOLDER = "./static"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == "GET":
